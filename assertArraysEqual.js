b3a1235c5da1f3526fa6c0e31ba9a68fe5feeba1
Sarah <sarah.mahov@gmail.com>
2019-08-26 20:56:43 +0000

    refactored assertArrayEqual function with separate test file

---
+++ assertArraysEqual.js
@@ -0,0 +1,11 @@
+const eqArrays = require("./eqArrays");
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+module.exports = assertArraysEqual;
