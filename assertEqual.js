cec2ced7d63da75eca2f7668d12796a9217e9fb2
Sarah <sarah.mahov@gmail.com>
2019-08-26 20:23:10 +0000

    refactored assertEqual with separate test file

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙂 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`☹️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+module.exports = assertEqual;
